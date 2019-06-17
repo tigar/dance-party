cb4e0f9658bf6d3f64e664158f8c0ece7fea3d5f
tigar <aatigar@gmail.com>
2019-06-16 20:20:17 -0700

    Server updates, working on the async

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Datastore = require('nedb');
 const fetch = require('node-fetch');
 const dotenv = require('dotenv').config()
 require('dotenv').config();
+var SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,8 +18,6 @@ app.use(express.json({ limit: '1mb' }));
 const database = new Datastore('database.db');
 database.loadDatabase();
 
-var SpotifyWebApi = require('spotify-web-api-node');
-
 var clientId = process.env.API_KEY,
   clientSecret = process.env.API_SECRET;
 
@@ -28,60 +27,31 @@ var spotifyApi = new SpotifyWebApi({
   clientSecret: clientSecret
 });
 
-// Get the token using the credentials
-spotifyApi.clientCredentialsGrant().then(
-  function(data) {
-    console.log('The token expires in ' + data.body['expires_in']);
-    console.log('The token is ' + data.body['access_token']);
 
-    // Save the token
-    spotifyApi.setAccessToken(data.body['access_token']);
 
-    // Get info about the playlist and log it
+app.get('/api', function(request, response) {
+  var playlist_data;
+  spotifyApi.clientCredentialsGrant().then(
+    function(data) {
+      console.log('The token expires in ' + data.body['expires_in']);
+      console.log('The token is ' + data.body['access_token']);
+  
+      // Save the token
+      spotifyApi.setAccessToken(data.body['access_token']);
+  
+      // Get info about the playlist and log it
     spotifyApi.getPlaylist(process.env.PLAYLIST_ID)
-  .then(function(data) {
-    console.log('Some information about this playlist', data.body);
-  }, function(err) {
-    console.log('Something went wrong fetching the playlist!', err);
-  });
-  },
-  function(err) {
-    console.log('Error: Could not get access token', err);
-  }
-);
-
-
-
-app.get('/api', (request, response) => {
-  database.find({}, (err, data) => {
-    if (err) {
-      response.end();
-      return;
+    .then(function(data) {
+      console.log('Here is the playlist:', data.body['expires_in']);
+      playlist_data = data.body;
+    }, function(err) {
+      console.log('Something went wrong fetching the playlist!', err);
+    });
+    },
+    function(err) {
+      console.log('Error: Could not get access token', err);
     }
-    response.json(data);
-  });
-});
-
-app.post('/api', (request, response) => {
-  const data = request.body;
-  const timestamp = Date.now();
-  data.timestamp = timestamp;
-  database.insert(data);
-  response.json(data);
-});
-
-app.get('/weather/:latlon', async (request, response) => {
-  console.log(request.params);
-
-  const api_key = process.env.API_KEY;
-  const api_secret = process.env.API_SECRET;
-
-  const spotify_url = `https://api.spotify.com/v1/${api_key}/`;
-  const spotify_response = await fetch(spotify_url);
-  const spotify_data = await spotify_response.json();
-
-  const data = {
-    air_quality: spotify_data
-  };
-  response.json(data);
+  );
+  console.log("I am at the end of the line");
+  response.json(data.body);
 });
